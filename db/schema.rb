aa56640d7523b4e4d2495c9ce8f6e9eebb316620
test <test>
2018-03-30 13:46:08 +0900

    added name column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330042242) do
+ActiveRecord::Schema.define(version: 20180330044211) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
